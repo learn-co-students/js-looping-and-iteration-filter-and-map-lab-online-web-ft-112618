2be2faca6e83954517265304435aeedc6b374c35
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-13 19:07:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {return driver.revenue > revenue})
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {return driver.name})
+}
+
+function exactMatch(drivers, attributes) {
+  return drivers.filter(function (driver) {
+    for (const key in attributes) { return driver[key] === attributes[key]}
+  })
+}
+
+function exactMatchToList(drivers, attributes) {
+  return exactMatch(drivers, attributes).map(function (driver) {return driver.name})
+}
